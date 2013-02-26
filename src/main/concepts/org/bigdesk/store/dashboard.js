712cb687df8f734dbf6807cd6a94790f768b390f
Lukas Vlcek <lukas.vlcek@gmail.com>
2013-02-26 17:28:32 +0100

    update dashboard concept

--- src/main/concepts/org/bigdesk/store/dashboard.js
+++ src/main/concepts/org/bigdesk/store/dashboard.js
@@ -28,57 +28,89 @@ goog.require('goog.dom.TagName');
 
 /**
  * @param {!Element} element to host the dashboard
+ * @param {!Element} endpoint
  * @param {!Element} start_button
  * @param {!Element} stop_button
  * @param {!Element} interval
- * @param {Object=} opt_conf
  * @constructor
  * @extends {goog.events.EventTarget}
  */
-org.bigdesk.store.Dashboard = function(element, start_button, stop_button, interval, opt_conf) {
+org.bigdesk.store.Dashboard = function(element, endpoint, start_button, stop_button, interval) {
 
     goog.events.EventTarget.call(this);
 
     this.element_ = element;
+    this.endpoint_ = endpoint;
     this.start_ = start_button;
     this.stop_ = stop_button;
     this.interval_ = interval;
 
-    this.prepareHTML();
+    var parts = this.prepareChart();
 
-    var manager = new org.bigdesk.store.Manager(opt_conf);
+    var getConf = function(endpoint, interval) {
+        var c = {
+            endpoint: endpoint.value,
+            delay: interval.value
+        };
+        console.log(c);
+        return c;
+    }
 
-    this.startId_     = goog.events.listen(this.start_,    'click',  function(){ manager.start() });
-    this.stopId_      = goog.events.listen(this.stop_,     'click',  function(){ manager.stop() });
-    this.intervalId_  = goog.events.listen(this.interval_, 'change', function(){
-        var newDelay = this.options[this.selectedIndex].value;
-        manager.updateDelay(newDelay);
-    }, false, this.interval_);
+    this.manager = new org.bigdesk.store.Manager(getConf(this.endpoint_, this.interval_));
 
-    this.nodeInfoAddId_ = goog.events.listen(manager, org.bigdesk.store.event.EventType.NODES_INFO_ADD, function(t) {
-//        console.log('node info', t.getData(), manager.getNodesInfoCount());
-        console.log('node info', manager.getNodesInfoCount());
-    });
-    this.nodeStatsAddId_ = goog.events.listen(manager, org.bigdesk.store.event.EventType.NODES_STATS_ADD, function(t) {
-//        console.log('node stats', t.getData(), manager.getNodesStatsCount());
-        console.log('node stats', manager.getNodesStatsCount());
-    });
-    this.clusterStateAddId_ = goog.events.listen(manager, org.bigdesk.store.event.EventType.CLUSTER_STATE_ADD, function(t) {
-//        console.log('cluster state', t.getData(), manager.getClusterStatesCount());
-        console.log('cluster state', manager.getClusterStatesCount());
-    });
-    this.clusterHealthAddId_ = goog.events.listen(manager, org.bigdesk.store.event.EventType.CLUSTER_HEALTH_ADD, function(t) {
-//        console.log('cluster health', t.getData(), manager.getClusterHealthCount());
-        console.log('cluster health', manager.getClusterHealthCount());
-    });
-    this.indexSegmentsAddId_ = goog.events.listen(manager, org.bigdesk.store.event.EventType.INDEX_SEGMENTS_ADD, function(t) {
-//        console.log('index segments', t.getData(), manager.getIndexSegmentsCount());
-        console.log('index segments', manager.getIndexSegmentsCount());
-    });
-    this.hotThreadsAddId_ = goog.events.listen(manager, org.bigdesk.store.event.EventType.HOT_THREADS_ADD, function(t) {
-//        console.log('hot threads', t.getData(),  manager.getHotThreadsCount());
-        console.log('hot threads', manager.getHotThreadsCount());
-    });
+    this.startId_ = goog.events.listen(this.start_, 'click',
+        function(){
+            if (this.endpoint_.value != this.manager.getEndpointUri()) {
+                if (this.manager.isRunning()) {
+                    this.manager.stop();
+                }
+                this.manager.dispose();
+                console.log('url changed - getting new manager');
+                this.manager = new org.bigdesk.store.Manager(getConf(this.endpoint_, this.interval_));
+            }
+            this.manager.start();
+        }, false, this);
+    this.stopId_      = goog.events.listen(this.stop_, 'click',
+        function(){
+            this.manager.stop();
+        }, false, this);
+    this.intervalId_  = goog.events.listen(this.interval_, 'change',
+        function(){
+            console.log('interval changed');
+            var newDelay = this.options[this.selectedIndex].value;
+            this.manager.updateDelay(newDelay);
+        }, false, this.interval_);
+
+    this.nodeInfoAddId_ = goog.events.listen(this.manager, org.bigdesk.store.event.EventType.NODES_INFO_ADD,
+        goog.bind(function(t) {
+//            console.log('node info', t.getData(), manager.getNodesInfoCount());
+            console.log('node info', this.manager.getNodesInfoCount());
+        }, this));
+    this.nodeStatsAddId_ = goog.events.listen(this.manager, org.bigdesk.store.event.EventType.NODES_STATS_ADD,
+        goog.bind(function(t) {
+//            console.log('node stats', t.getData(), manager.getNodesStatsCount());
+            console.log('node stats', this.manager.getNodesStatsCount());
+        }, this));
+    this.clusterStateAddId_ = goog.events.listen(this.manager, org.bigdesk.store.event.EventType.CLUSTER_STATE_ADD,
+        goog.bind(function(t) {
+//            console.log('cluster state', t.getData(), manager.getClusterStatesCount());
+            console.log('cluster state', this.manager.getClusterStatesCount());
+        }, this));
+    this.clusterHealthAddId_ = goog.events.listen(this.manager, org.bigdesk.store.event.EventType.CLUSTER_HEALTH_ADD,
+        goog.bind(function(t) {
+//            console.log('cluster health', t.getData(), manager.getClusterHealthCount());
+            console.log('cluster health', this.manager.getClusterHealthCount());
+        }, this));
+    this.indexSegmentsAddId_ = goog.events.listen(this.manager, org.bigdesk.store.event.EventType.INDEX_SEGMENTS_ADD,
+        goog.bind(function(t) {
+//            console.log('index segments', t.getData(), manager.getIndexSegmentsCount());
+            console.log('index segments', this.manager.getIndexSegmentsCount());
+        }, this));
+    this.hotThreadsAddId_ = goog.events.listen(this.manager, org.bigdesk.store.event.EventType.HOT_THREADS_ADD,
+        goog.bind(function(t) {
+//            console.log('hot threads', t.getData(),  manager.getHotThreadsCount());
+            console.log('hot threads', this.manager.getHotThreadsCount());
+        }, this));
 };
 goog.inherits(org.bigdesk.store.Dashboard, goog.events.EventTarget);
 
@@ -98,14 +130,16 @@ org.bigdesk.store.Dashboard.prototype.disposeInternal = function() {
     goog.events.unlistenByKey(this.hotThreadsAddId_);
 
     delete this.element_;
+    delete this.endpoint_;
     delete this.start_;
     delete this.stop_;
 };
 
 /**
+ * @return {*}
  * @private
  */
-org.bigdesk.store.Dashboard.prototype.prepareHTML = function() {
+org.bigdesk.store.Dashboard.prototype.prepareChart = function() {
 
     var storeParts = [
         {name:'Nodes Info',     id:'nodeInfo'},
@@ -156,5 +190,6 @@ org.bigdesk.store.Dashboard.prototype.prepareHTML = function() {
 
     var parts = {};
     svg.selectAll('.storePart').datum(function(){parts[this.id]=this});
-    console.log(parts);
+
+    return parts;
 };
